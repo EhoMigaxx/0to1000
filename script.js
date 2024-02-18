ea777c545187c261a58d0be600316f4d06dcac67
Miguel Babo <138327344+MiguelBabo@users.noreply.github.com>
2024-02-18 16:23:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,71 @@
-const userNumber = document.getElementById("userNumber");
-const hint1 = document.getElementById("hint1");
-const hint2 = document.getElementById("hint2");
-const goBtn = document.getElementById("go");
+const mode = document.querySelectorAll(".mode");
+const hint = document.getElementById('hint');
+const guess = document.getElementById('guess');
+const user_input = document.getElementById('user_input');
 
-var num = Math.floor(Math.random() * 1000) + 1;
+// const easy = document.getElementById('easy')
+// const normal = document.getElementById('normal')
+// const hard = document.getElementById('hard')
 
-const y = document.getElementById("userNumber")
+var dificulty = ''
+
+function easy(){
+    sessionStorage.setItem("dificulty", 0);
+    window.location.reload()
+}
+function normal(){
+    sessionStorage.setItem("dificulty", 1);
+    window.location.reload()
+}
+function hard(){
+    sessionStorage.setItem("dificulty", 2);
+    window.location.reload()
+}
+
+var number
+
+if(sessionStorage.getItem("dificulty") == 0){
+    number = Math.floor(Math.random() * 10) + 1;
+    document.getElementById('mode').textContent = "10"
+}
+
+if(sessionStorage.getItem("dificulty") == 1){
+    number = Math.floor(Math.random() * 1000) + 1;
+    document.getElementById('mode').textContent = "1000"
+}
+
+if(sessionStorage.getItem("dificulty") == 2){
+    number = Math.floor(Math.random() * 1000000) + 1;
+    document.getElementById('mode').textContent = "1000000"
+}
+
+const y = document.getElementById("user_input")
 y.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
-      sendNumber()
+      guessNumber()
       blink()
     }
 });
 
-function sendNumber(){
-    const x = document.getElementById("userNumber").value;
+// console.log(dificulty)
+// console.log(number);
 
-    if(x > num){
-        hint2.innerHTML = "The number is lower!"
+function guessNumber(){
+    if(user_input.value == number){
+        hint.innerHTML = 'You got it!'
     }
-    if(x < num){
-        hint2.innerHTML = "The number is higher!"
+
+    if(user_input.value < number){
+        hint.innerHTML = `The number is higher than ${user_input.value}`
     }
-    if(x == num){
-        hint2.innerHTML = "You got it!"
+
+    if(user_input.value > number){
+        hint.innerHTML = `The number is lower than ${user_input.value}`
     }
 }
 
 function blink(){
-    hint2.animate([
+    hint.animate([
         { opacity: '0' },
         { opacity: '1' }
     ], {
@@ -36,8 +73,5 @@ function blink(){
     })
 }
 
-goBtn.addEventListener("click", sendNumber)
-goBtn.addEventListener("click", blink)
-
-
-
+guess.addEventListener('click', guessNumber)
+guess.addEventListener('click', blink)
